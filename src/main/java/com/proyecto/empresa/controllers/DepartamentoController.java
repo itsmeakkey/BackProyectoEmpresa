b474fbd2cd9fb4e77711a307b0bb1f81c3bf9e19
Jose David Rodriguez Gallardo <jrgallar@emeal.nttdata.com>
2024-11-06 13:52:41 +0100

    Crontrolador departamento finalizado.

--- src/main/java/com/proyecto/empresa/controllers/DepartamentoController.java
+++ src/main/java/com/proyecto/empresa/controllers/DepartamentoController.java
@@ -1,8 +1,12 @@
 package com.proyecto.empresa.controllers;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,9 +26,22 @@ public class DepartamentoController {
 	//Endpoint para obtener todos los departamentos
     @GetMapping
     public List<Departamento> getAllDepartamentos() {
-        return departamentoServices.getAllDepartamentos();
+        return this.departamentoServices.getAllDepartamentos();
     }
- 
+    
+    //Endpoint para buscar por id un departamento
+    @GetMapping(path = "/{id}")
+    public Optional<Departamento> findById(@PathVariable("Id") Long id) {
+        return this.departamentoServices.findById(id);
+    }
+    
+    //Crear un departamento nuevo . @RequestBody convierte el json a Departamento en la petición
+    @PostMapping()
+    public Departamento saveDepartamento(@RequestBody Departamento d) {
+    return this.departamentoServices.saveDepartamento(d);
+    }
+    
+    
  
 
 }
--- src/main/java/com/proyecto/empresa/services/DepartamentoServices.java
+++ src/main/java/com/proyecto/empresa/services/DepartamentoServices.java
@@ -2,6 +2,7 @@ package com.proyecto.empresa.services;
  
 import java.util.List;
 import java.util.Optional;
+
 import com.proyecto.empresa.models.Departamento;
 import com.proyecto.empresa.repositories.DepartamentoRepository;
 
@@ -13,15 +14,18 @@ public class DepartamentoServices {
 	public DepartamentoServices(DepartamentoRepository departamentoRepository) {
 		this.departamentoRepository = departamentoRepository;
 	}
-    // Obtener todos los departamentos
+    //Obtener todos los departamentos
     public List<Departamento> getAllDepartamentos() {
         return departamentoRepository.findAll();
     }
  
-    // Buscar un departamento por ID
+    //Buscar un departamento por ID
     public Optional<Departamento> findById(Long id) {
         return departamentoRepository.findById(id);
     }
 	
-
+    //Crear un nuevo departamento
+    public Departamento saveDepartamento(Departamento d) {
+    	return departamentoRepository.save(d);
+    }
 }
