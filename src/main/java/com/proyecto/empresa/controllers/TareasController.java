61368af42d1deba456793c11e16e1b867fa20345
Jose David Rodriguez Gallardo <jrgallar@emeal.nttdata.com>
2024-11-08 14:59:42 +0100

    Metodos de tareas terminados(falta uno)

--- src/main/java/com/proyecto/empresa/controllers/TareasController.java
+++ src/main/java/com/proyecto/empresa/controllers/TareasController.java
@@ -1,5 +1,6 @@
 package com.proyecto.empresa.controllers;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,33 +26,105 @@ public class TareasController {
 
 	@Autowired
 	public TareasController(TareasServices tareasServices) {
-	this.tareasServices = tareasServices;
+		this.tareasServices = tareasServices;
 	}
-	
-	//MÉTODOS COMUNES	
-		//Endpoint para obtener todos las tareas
-		@GetMapping
-	    public List<Tarea> getAllTareas() {
-	        return this.tareasServices.getAll();
-	    }
-		
-		//Endpoint para buscar tareas por Id
-		@GetMapping(path = "/{id}")
-		public Optional<Tarea> findById(@PathVariable("id") Long id){
-			return this.tareasServices.findById(id);
+
+	// MÉTODOS COMUNES
+	// Endpoint para obtener todos las tareas
+	@GetMapping
+	public List<Tarea> getAllTareas() {
+		return this.tareasServices.getAll();
+	}
+
+	// Endpoint para buscar tareas por Id
+	@GetMapping(path = "/{id}")
+	public Optional<Tarea> findById(@PathVariable("id") Long id) {
+		return this.tareasServices.findById(id);
+	}
+
+	// MÉTODOS PROPIOS
+
+	/* CREAR */
+	@PostMapping()
+	public ResponseEntity<Tarea> createTarea(@RequestBody TareaTO tareaTo) {
+		// Llamada al servicio para guardar la tarea
+		Tarea createTarea = tareasServices.createTarea(tareaTo);
+
+		// Devuelve una respuesta 201
+		return new ResponseEntity<>(createTarea, HttpStatus.CREATED);
+	}
+
+	// Endpoint para buscar tareas asignadas a un empelado específico
+	@GetMapping(path = "/tarea/{empleadoId}")
+	public ResponseEntity<List<Tarea>> findByEmpleadoId(@PathVariable("empleadoId") Long id) {
+		// Llamada al servicio para buscar la tarea
+		List<Tarea> tarea = tareasServices.findByEmpleadoId(id);
+		// Si no encuentra la tarea, devuelve un 404. Si existe, devuelve un 200
+		if (tarea.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(tarea, HttpStatus.OK);
+	}
+
+	// Endpoint para buscar tareas entregadas a tiempo(true)
+	@GetMapping(path = "/tarea/true")
+	public ResponseEntity<List<Tarea>> findByEntregaATiempo() {
+		// Llamada al servicio para buscar la tarea
+		List<Tarea> tarea = tareasServices.findByEntregaATiempo();
+		// Si no encuentra la tarea, devuelve un 404. Si existe, devuelve un 200
+		if (tarea.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(tarea, HttpStatus.OK);
+	}
+
+	// Endpoint para buscar tareas entregadas a destiempo(false)
+	@GetMapping(path = "/tarea/false")
+	public ResponseEntity<List<Tarea>> findByEntregaADestiempo() {
+		// Llamada al servicio para buscar la tarea
+		List<Tarea> tarea = tareasServices.findByEntregaADestiempo();
+		// Si no encuentra la tarea, devuelve un 404. Si existe, devuelve un 200
+		if (tarea.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(tarea, HttpStatus.OK);
+	}
+
+	// Endpoint para buscar tareas no finalizadas
+	@GetMapping(path = "/tarea/nofinalizada")
+	public ResponseEntity<List<Tarea>> findByNotFinished() {
+		// Llamada al servicio para buscar la tarea
+		List<Tarea> tarea = tareasServices.findByNotFinished();
+		// Si no encuentra la tarea, devuelve un 404. Si existe, devuelve un 200
+		if (tarea.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(tarea, HttpStatus.OK);
+	}
+//PENDIENTES DE ARREGLAR
+	// Endpoint para buscar tareas entregadas después de una fecha específica
+	@GetMapping(path = "/fecha/{fechaPos}")
+		public ResponseEntity<List<Tarea>>findByFechaCreacion(@PathVariable("fecha")Date fechaCreacion) {
+			// Llamada al servicio para buscar la tarea
+			List<Tarea> tarea = tareasServices.findByFechaCreacion(fechaCreacion);
+			// Si no encuentra la tarea, devuelve un 404. Si existe, devuelve un 200
+			if (tarea.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+			return new ResponseEntity<>(tarea, HttpStatus.OK);
+		}
+	//PENDIENTES DE ARREGLAR
+	// Endpoint para buscar tareas entregadas antes de una fecha específica
+	@GetMapping(path = "/fecha/{fechaAnt}")
+		public ResponseEntity<List<Tarea>>findByFechaAnteriorCreacion(@PathVariable("fecha")Date fechaCreacion) {
+			// Llamada al servicio para buscar la tarea
+			List<Tarea> tarea = tareasServices.findByFechaAnteriorCreacion(fechaCreacion);
+			// Si no encuentra la tarea, devuelve un 404. Si existe, devuelve un 200
+			if (tarea.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+			return new ResponseEntity<>(tarea, HttpStatus.OK);
 		}
 		
-	//MÉTODOS PROPIOS 
-		
-		 /*CREAR */
-		@PostMapping()
-		public ResponseEntity<Tarea> createTarea(@RequestBody TareaTO tareaTo){
-			//Llamada al servicio para guardar la tarea
-			Tarea createTarea = tareasServices.createTarea(tareaTo);
-			
-			//Devuelve una respuesta 201
-			return new ResponseEntity<>(createTarea, HttpStatus.CREATED);
-		}
-	    
-	    
+
 }
--- src/main/java/com/proyecto/empresa/services/TareasServices.java
+++ src/main/java/com/proyecto/empresa/services/TareasServices.java
@@ -1,5 +1,5 @@
 package com.proyecto.empresa.services;
- 
+
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -9,92 +9,89 @@ import com.proyecto.empresa.models.Tarea;
 import com.proyecto.empresa.repositories.EmpleadoRepository;
 import com.proyecto.empresa.repositories.TareasRepository;
 import com.proyecto.empresa.to.TareaTO;
- 
 
 public class TareasServices {
- 
-	//No se necesita Autowired porque manejamos las inyecciones con beans desde AppConfig
-    private final TareasRepository tareasRepository;
-    private final EmpleadoRepository empleadoRepository;
-    
-    public TareasServices(TareasRepository tareasRepository, EmpleadoRepository empleadoRepository) {
-        this.tareasRepository = tareasRepository;
-        this.empleadoRepository = empleadoRepository;
-    }
- 
-    //MÉTODOS COMUNES
-    //Obtiene todas las tareas
-    public List<Tarea> getAll() {
-        return tareasRepository.findAll();
-    }
- 
-    //Busca tarea por ID
-    public Optional<Tarea> findById(Long id) {
-        return tareasRepository.findById(id);
-    }
- 
-    //MÉTODOS PROPIO
-    //Crea una nueva tarea
-    public Tarea createTarea(TareaTO t) {
-    	Empleado empleado =empleadoRepository.findById(t.getEmpleado().getId())
-    			.orElseThrow(() -> new RuntimeException("Empleado no encontrado"));
-    	t.setEmpleado(empleado);
-    	
-    	//Creamos la tarea recibiendo los valores de TareaTO
-    	Tarea tarea = new Tarea();
-    	tarea.setNombreTarea(t.getNombreTarea());
-    	tarea.setFechaCreacion(t.getFechaCreacion());
-    	tarea.setFechaFin(t.getFechaFin());
-    	tarea.setEntregadoATiempo(t.getEntregadoATiempo());
-    	tarea.setFechaEstimada(t.getFechaEstimada());
-    	
-    	//Asigna tarea al empleado 
-    	tarea.setEmpleado(empleado);
-    	
-    	//Guardamos la tarea en BBDD
-    return tareasRepository.save(tarea);
-    }
-   
- 
-    //Elimina una tarea por ID
-    public void delete(Long id) {
-        tareasRepository.deleteById(id);
-    }
- 
-    //Busca tareas asignadas a un empleado específico
-    public List<Tarea> findByEmpleadoId(Long empleadoId) {
-        return tareasRepository.findByEmpleadoId(empleadoId);
-    }
- 
-    //Busca tareas entregadas a tiempo
-    public List<Tarea> findByEntregaATiempo() {
-        return tareasRepository.findByEntregadoATiempoTrue();
-    }
- 
-    //Busca tareas entregadas fuera de tiempo
-    public List<Tarea> findByEntregaADestiempo() {
-        return tareasRepository.findByEntregadoATiempoFalse();
-    }
- 
-    //Busca tareas no finalizadas (fecha de fin es null)
-    public List<Tarea> findByNotFinished() {
-        return tareasRepository.findByFechaFinIsNull();
-    }
- 
-    //Busca tareas creadas después de una fecha específica
-    public List<Tarea> findByFechaCreacion(Date fecha) {
-        return tareasRepository.findByFechaCreacionAfter(fecha);
-    }
- 
-    // Busca tareas creadas antes de una fecha específica
-    public List<Tarea> findByFechaAnteriorCreacion(Date fecha) {
-        return tareasRepository.findByFechaCreacionBefore(fecha);
-    }
- 
-    // Busca tareas creadas entre dos fechas específicas
-    public List<Tarea> findByFechaEntreCreacion(Date fechaInicio, Date fechaFin) {
-        return tareasRepository.findByFechaCreacionBetween(fechaInicio, fechaFin);
-    }
-    
+
+	// No se necesita Autowired porque manejamos las inyecciones con beans desde
+	// AppConfig
+	private final TareasRepository tareasRepository;
+	private final EmpleadoRepository empleadoRepository;
+
+	public TareasServices(TareasRepository tareasRepository, EmpleadoRepository empleadoRepository) {
+		this.tareasRepository = tareasRepository;
+		this.empleadoRepository = empleadoRepository;
+	}
+
+	// MÉTODOS COMUNES
+	// Obtiene todas las tareas
+	public List<Tarea> getAll() {
+		return tareasRepository.findAll();
+	}
+
+	// Busca tarea por ID
+	public Optional<Tarea> findById(Long id) {
+		return tareasRepository.findById(id);
+	}
+
+	// MÉTODOS PROPIO
+	// Crea una nueva tarea
+	public Tarea createTarea(TareaTO t) {
+		Empleado empleado = empleadoRepository.findById(t.getEmpleado().getId())
+				.orElseThrow(() -> new RuntimeException("Empleado no encontrado"));
+		t.setEmpleado(empleado);
+
+		// Creamos la tarea recibiendo los valores de TareaTO
+		Tarea tarea = new Tarea();
+		tarea.setNombreTarea(t.getNombreTarea());
+		tarea.setFechaCreacion(t.getFechaCreacion());
+		tarea.setFechaFin(t.getFechaFin());
+		tarea.setEntregadoATiempo(t.getEntregadoATiempo());
+		tarea.setFechaEstimada(t.getFechaEstimada());
+
+		// Asigna tarea al empleado
+		tarea.setEmpleado(empleado);
+
+		// Guardamos la tarea en BBDD
+		return tareasRepository.save(tarea);
+	}
+
+	/*
+	 * Elimina una tarea por ID public void delete(Long id) {
+	 * tareasRepository.deleteById(id); }
+	 */
+	// Busca tareas asignadas a un empleado específico
+	public List<Tarea> findByEmpleadoId(Long empleadoId) {
+		return tareasRepository.findByEmpleadoId(empleadoId);
+	}
+
+	// Busca tareas entregadas a tiempo
+	public List<Tarea> findByEntregaATiempo() {
+		return tareasRepository.findByEntregadoATiempoTrue();
+	}
+
+	// Busca tareas entregadas fuera de tiempo
+	public List<Tarea> findByEntregaADestiempo() {
+		return tareasRepository.findByEntregadoATiempoFalse();
+	}
+
+	// Busca tareas no finalizadas (fecha de fin es null)
+	public List<Tarea> findByNotFinished() {
+		return tareasRepository.findByFechaFinIsNull();
+	}
+
+	// Busca tareas creadas después de una fecha específica
+	public List<Tarea> findByFechaCreacion(Date fecha) {
+		return tareasRepository.findByFechaCreacionAfter(fecha);
+	}
+
+	// Busca tareas creadas antes de una fecha específica
+	public List<Tarea> findByFechaAnteriorCreacion(Date fecha) {
+		return tareasRepository.findByFechaCreacionBefore(fecha);
+	}
+
+	// Busca tareas creadas entre dos fechas específicas
+	public List<Tarea> findByFechaEntreCreacion(Date fechaInicio, Date fechaFin) {
+		return tareasRepository.findByFechaCreacionBetween(fechaInicio, fechaFin);
+	}
 
 }
